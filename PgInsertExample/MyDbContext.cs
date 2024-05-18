3122b919cef722c20cf503b5eab68c802fb9f4ae
Franck Pachot (YB) <mail@pachot.net>
2024-05-18 20:23:26 +0000

    ServerCompatibilityMode

--- PgInsertExample/MyDbContext.cs
+++ PgInsertExample/MyDbContext.cs
@@ -4,7 +4,7 @@ public class MyDbContext : DbContext
     public DbSet<MyTable> MyTable { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Host=localhost;Username=yugabyte;Password=yugabyte;Database=yugabyte;Pooling=true;Minimum Pool Size=1;Maximum Pool Size=20;No Reset On Close=false;Multiplexing=false;Max Auto Prepare=10");
+        optionsBuilder.UseNpgsql("Host=localhost;Username=yugabyte;Password=yugabyte;Database=yugabyte;Pooling=true;Minimum Pool Size=1;Maximum Pool Size=20;No Reset On Close=false;Multiplexing=false;Max Auto Prepare=10;ServerCompatibilityMode=");
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
