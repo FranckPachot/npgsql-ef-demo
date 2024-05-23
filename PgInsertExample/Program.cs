6e5b2a7bb5cddd7e706208a5ec0e59815b69da91
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2024-05-23 17:50:57 +0000

    add tcpdump

--- PgInsertExample/Program.cs
+++ PgInsertExample/Program.cs
@@ -8,28 +8,35 @@ class Program
     {
         using (var context = new MyDbContext())
         {
-
             // Ensure database is created
             context.Database.EnsureCreated();
-
             // Reset pg_stat_statements
             context.Database.ExecuteSqlRaw("SELECT pg_stat_statements_reset();");
+            //end connection
+            context.Database.CloseConnection();
+        }
 
-            for (int i = 0; i < 10; i++)
-            {
-            // Insert 1000 rows
-            var entries = Enumerable.Range(1, 1000).Select(i => new MyTable { value = $"Value {i}" }).ToList();
-            context.MyTable.AddRange(entries);
-            context.SaveChanges();
-            //context.BulkSaveChanges();
-            Console.WriteLine("1000 rows inserted into my_table.");  
-            }
-
+        for (int i = 0; i < 1000; i++)
+        {
+            using (var context = new MyDbContext())
+                    {
+                        // Insert 1000 rows
+                        var entries = Enumerable.Range(1, 1000).Select(i => new MyTable { value = $"Value {i}" }).ToList();
+                        context.MyTable.AddRange(entries);
+                        context.SaveChanges();
+                        // context.BulkSaveChanges(); // Uncomment if using BulkExtensions or similar library
+                        Console.WriteLine("1000 rows inserted into MyTable.");
+                        context.Database.CloseConnection();
+                    }
+        }
+        
+        using (var context = new MyDbContext())
+        {    
 
             // Execute native SQL and display the rows
             using (var command = context.Database.GetDbConnection().CreateCommand())
             {
-                command.CommandText = @"SELECT calls, rows, total_time, query FROM pg_stat_statements WHERE calls>@CallsThreshold ORDER BY calls, total_time";
+                command.CommandText = @"SELECT calls, rows, total_time, query FROM pg_stat_statements WHERE calls > @CallsThreshold ORDER BY calls, total_time";
                 context.Database.OpenConnection();
                 var parameter = command.CreateParameter();
                 parameter.ParameterName = "@CallsThreshold";
@@ -39,14 +46,11 @@ class Program
                 {
                     while (result.Read())
                     {
-                        Console.WriteLine($"{result.GetInt64(0),10} calls, {result.GetInt64(1),10} rows, {Math.Round(result.GetDouble(2),2),10} ms, Query: {result.GetString(3)}");
+                        Console.WriteLine($"{result.GetInt64(0),10} calls, {result.GetInt64(1),10} rows, {Math.Round(result.GetDouble(2), 2),10} ms, Query: {result.GetString(3)}");
                     }
                 }
-                context.Database.CloseConnection();
             }
-
-            
-        }
-
+            context.Database.CloseConnection();
+        }        
     }
 }
--- README.md
+++ README.md
@@ -25,7 +25,7 @@ dotnet run
 start YugabyteDB on same port / address as PostgreSQL
 
 ```
-docker run -d -p5432:5433 -p 15433:15433 yugabytedb/yugabyte yugabyted start --background=false --tserver_flags=yb_enable_read_committed_isolation=true,ysql_pg_conf_csv="{shared_preload_libraries='auto_explain',log_statement='all'}"
+docker run --name yb -d -p5432:5433 -p 15433:15433 yugabytedb/yugabyte yugabyted start --background=false --tserver_flags=yb_enable_read_committed_isolation=true,ysql_pg_conf_csv="{shared_preload_libraries='auto_explain',log_statement='all'}"
 
 type pg_isready 2>/dev/null && until pg_isready ; do sleep 1 ; done | uniq -c
 # tail -F /root/var/data/yb-data/tserver/logs/postgresql*
@@ -39,3 +39,21 @@ cd PgInsertExample
 dotnet run
 ```
 
+### Trace the RPCs to master:
+
+When in the YugabyteDB container you can use `tcpdump` to look at the RPCs to the `yb-master` (I used that to see the impact of `ServerCompatibilityMode=NoTypeLoading`)
+```
+docker exec -it yb bash
+
+tcpdump -nni any dst port 7100 -A |
+# display every 10 seconds the count of lines with ".yb."
+ awk -F . '
+ / IP [0-9.]+[.][0-9.]+ > [0-9.]+[.]7100: /{t1=$1}
+ /[.]yb[.]/{c[$0]=c[$0]+1}
+ substr(t1,1,7)>substr(t0,1,7){    # print every 10 seconds
+  for (i in c) printf "%10d %-s\n",c[i],i;print t1;delete(c)
+ }
+ {t0=t1}
+'
+
+```
